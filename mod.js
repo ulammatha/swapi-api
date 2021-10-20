58d9c551b956067ef52d0ad248b2b785b00dc1d8
Uday Kumar Lammatha <ulammatha1711@gmail.com>
2021-10-20 13:44:51 -0700

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,7 +1,7 @@
 import { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
 import axiod from "https://deno.land/x/axiod/mod.ts";
 
-async function handler(req: Request): Response {
+async function handler(req) {
   const swapi = await axiod.get("https://swapi.dev/api/people/1")
   console.log('<swapi', swapi.data)
   return new Response(JSON.stringify(swapi.data));
