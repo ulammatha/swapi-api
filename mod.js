d504d7aecbc51dd68b66fa9fa7516f58e0862973
Uday Kumar Lammatha <ulammatha1711@gmail.com>
2021-10-20 20:18:57 -0700

    use oak for static file return

--- mod.js
+++ mod.js
@@ -1,12 +1,32 @@
-import { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
-import axiod from "https://deno.land/x/axiod/mod.ts";
-const path = `${Deno.cwd()}/css/index.html`;
+// import { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
+// import axiod from "https://deno.land/x/axiod/mod.ts";
+// const path = `${Deno.cwd()}/css/index.html`;
+
+// async function handler(req) {
+//   const swapi = await axiod.get("https://swapi.dev/api/people/1")
+//   return new Response(path);
+// }
+
+// console.log("Listening on http://localhost:8000");
+// await listenAndServe(":8000", handler);
+
+import { Application } from 'https://deno.land/x/oak/mod.ts'
+
+const port = 8080
+
+const app = new Application()
 
 
-async function handler(req) {
-  const swapi = await axiod.get("https://swapi.dev/api/people/1")
-  return new Response(path);
-}
+app.use(async (context, next) => {
+    const root = `${Deno.cwd()}/css/index.html`
+    try {
+        await context.send({ root })
+    } catch {
+        next()
+    }
+})
 
-console.log("Listening on http://localhost:8000");
-await listenAndServe(":8000", handler);
+
+app.addEventListener("listen", ({ port }) => console.log(`listening on port: ${port}`) )
+
+await app.listen({ port })
