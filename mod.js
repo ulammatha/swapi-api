2ad73685c843fb9526840551cd16f8dd170ae0c3
Uday Kumar Lammatha <ulammatha1711@gmail.com>
2021-10-20 19:23:48 -0700

    fix index html path

--- mod.js
+++ mod.js
@@ -1,6 +1,6 @@
 import { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
 import axiod from "https://deno.land/x/axiod/mod.ts";
-const html. from "./css/index.html"
+import html from "./css/index.html";
 
 async function handler(req) {
   const swapi = await axiod.get("https://swapi.dev/api/people/1")
